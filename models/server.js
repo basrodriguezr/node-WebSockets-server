b09801e609e203eb11d5a58d73b2fa6822f4af96
bastian rodríguez <basrodriguezr@gmail.com>
2022-07-19 01:13:18 -0400

    conectando sockets

--- models/server.js
+++ models/server.js
@@ -1,18 +1,22 @@
 const express = require('express');
-const cors = require('cors')
-
+const cors = require('cors');
 
 
 class Server{
     constructor(){
             this.app = express();
             this.port = process.env.PORT;
+            this.server = require('http').createServer(this.app);
+            this.io = require('socket.io')(this.server);
+
             this.paths = {};        
 
             //middlewares
             this.middelwares();
 
             this.routes();
+            
+            this.sockets();
     }
 
     
@@ -28,8 +32,19 @@ class Server{
         //this.app.use('/api',require('./routes/api'));
     }
 
+    //sockets
+    sockets(){
+        this.io.on("connection", (socket) => {
+            console.log('Cliente conectado', socket.id);
+
+            socket.on('disconnect', () => {
+                console.log('Cliente desconectado', socket.id);
+            });
+        });
+    }
+
     listen(){
-        this.app.listen(this.port || 3000,()=>{
+        this.server.listen(this.port || 3000,()=>{
             console.log(`Listening at http://localhost:${this.port}`)
         });
     }
